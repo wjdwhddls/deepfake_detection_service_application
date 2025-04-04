7ee34d7bd99232cadb8e93f93d9a6c7442d6dfed
sungwoonko <ko_sw@naver.com>
2025-04-04 17:38:10 +0900

    feat : 회원가입 페이지 제작

--- src/screens/styles.js
+++ src/screens/styles.js
@@ -6,33 +6,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  content: {
-    flexGrow: 1,
-    justifyContent: 'center',
-    paddingVertical: 40,
-    width: '100%',
-    alignItems: 'center',
+    padding: 20,
   },
   title: {
-    fontSize: 32,
+    fontSize: 36,
     color: '#FFF',
     fontWeight: 'bold',
     marginBottom: 30,
     textAlign: 'center',
   },
   input: {
-    height: 60,                  // 높이 증가
-    width: '80%',                // 너비 설정
+    height: 60,
+    width: '100%',                  // 폭을 100%로 설정
     borderColor: '#A9A9A9',
     borderWidth: 1,
-    borderRadius: 10,
+    borderRadius: 25,               // 둥근 모서리
     paddingHorizontal: 15,
-    paddingVertical: 10,         // 수직 패딩 추가
-    backgroundColor: '#222',      // 배경 색상 추가
+    paddingVertical: 10,
+    backgroundColor: '#222',
     color: '#FFF',
     marginBottom: 15,
-    fontSize: 16,                // 폰트 크기 조정
+    fontSize: 16,
   },
   orText: {
     fontSize: 16,
@@ -42,9 +36,18 @@ const styles = StyleSheet.create({
   },
   authButton: {
     height: 55,
-    width: '80%',
-    borderRadius: 10,
-    backgroundColor: '#DB4437',
+    width: '100%',                  // 폭을 100%로 설정
+    borderRadius: 25,               // 둥근 모서리
+    backgroundColor: '#DB4437',     // 구글 버튼 색상
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 15,
+  },
+  naverButton: {
+    height: 55,
+    width: '100%',                  // 폭을 100%로 설정
+    borderRadius: 25,               // 둥글게
+    backgroundColor: '#1EC800',     // 네이버 색상
     justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 15,
@@ -57,6 +60,9 @@ const styles = StyleSheet.create({
     color: '#A9A9A9',
     textAlign: 'center',
     marginTop: 10,
+  },
+  linkText: {
+    color: '#DB4437', // 회원가입 텍스트 색상
   },
 });
 
